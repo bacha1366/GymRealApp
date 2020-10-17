6b96395fb4838ded40d2efdd02bd56955b31322a
bacha1366 <bacha9@yandex.ru>
2020-10-17 14:30:46 +0500

    Update ExeActivity.java

--- app/src/main/java/ru/bacha/gym/ExeActivity.java
+++ app/src/main/java/ru/bacha/gym/ExeActivity.java
@@ -10,6 +10,5 @@ public class ExeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_execises);
-	 int b = 8;
     }
 }
