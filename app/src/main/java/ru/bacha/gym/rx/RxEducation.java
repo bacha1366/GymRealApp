8795b5e1da9479b0fc83cfcb45691f3a028422cd
bacha1366 <bacha9@yandex.ru>
2020-10-29 01:44:02 +0500

    29/10

--- app/src/main/java/ru/bacha/gym/rx/RxEducation.java
+++ app/src/main/java/ru/bacha/gym/rx/RxEducation.java
@@ -1,5 +1,6 @@
 package ru.bacha.gym.rx;
 
+import java.util.Random;
 import java.util.function.Consumer;
 
 import io.reactivex.rxjava3.annotations.NonNull;
@@ -11,26 +12,35 @@ public class RxEducation {
 
     //этот метод должен вернуть Observable, который запаблишит число от 0 до 10
     Observable<Integer> getNumbersObservable() {
-        throw new IllegalStateException("Not implemented yet");
+        return Observable.range(0,11);
+        //throw new IllegalStateException("Not implemented yet");
     }
 
     //этот метод должен вернуть Observable, который опубликует числа от 0 до 10,
     // только в виде строк "0", "1" и т.д.
     // реализация должна использовать метод getNumbersObservable
     Observable<String> getNumberStringsObservable() {
-        throw new IllegalStateException("Not implemented yet");
+        return getNumbersObservable().map(String::valueOf);
+        //throw new IllegalStateException("Not implemented yet");
     }
 
     //этот метод должен вернуть Observable, который опубликует только ЧЕТНЫЕ числа от 0 до 10,
     // реализация должна использовать метод getNumbersObservable
     Observable<Integer> getEvenNumbersObservable() {
-        throw new IllegalStateException("Not implemented yet");
+        return getNumbersObservable().filter(i -> i % 2 == 0);
+        //throw new IllegalStateException("Not implemented yet");
     }
 
     //этот метод должен подписаться на переданный observable и все данные из него передать в функцию nextFunction,
     //а если случится exception = передать его в errorFunction
-    void publishAllEvents(Observable<Integer> observable, Consumer<Integer> nextFunction, Consumer<Throwable> errorFunction) {
-        throw new IllegalStateException("Not implemented yet");
+    void publishAllEvents(
+            Observable<Integer> observable,
+            Consumer<Integer> nextFunction,
+            Consumer<Throwable> errorFunction
+    ) {
+        observable.subscribe(integer -> nextFunction.accept(integer),throwable ->
+                errorFunction.accept(throwable));
+        //throw new IllegalStateException("Not implemented yet");
     }
 
 
