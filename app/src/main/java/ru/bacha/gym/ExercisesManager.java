4d5c2d3c039330de307a7d8679a7f85b7466ecd9
mksm <mksmdavydoff@gmail.com>
2020-10-29 10:55:01 +0300

    ExercisesManager update

--- app/src/main/java/ru/bacha/gym/ExercisesManager.java
+++ app/src/main/java/ru/bacha/gym/ExercisesManager.java
@@ -1,10 +1,11 @@
 package ru.bacha.gym;
 
-import android.content.Context;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
 
 public class ExercisesManager {
 
@@ -22,15 +23,15 @@ public class ExercisesManager {
     };
 
 
-    public Exercise[] getMyExercises() {
-        return myExercises;
+    public Single<List<Exercise>> getMyExercises() {
+        return null;
     }
 
 
     //есть два способа ответа на неправильно введенный id. Первый - возвращать null вместо Exercise,
     // второй - выбрасывать ошибку. Мне больше нравится второй, потому что он сразу дает понять, что
     // мы что-то сделали не так.
-    public Exercise getExerciseForID(int id) {
+    public Single<Exercise> getExerciseForID(int id) {
         //int i;
         for (int i = 0; i < myExercises.length; i++) {
             if (id == myExercises[i].id) return myExercises[i];
@@ -38,7 +39,11 @@ public class ExercisesManager {
         throw new IllegalArgumentException("Не могу найти упражнение с id" + id);
     }
 
-    public void saveApproach(int id, double weight, int replay){
+    public Completable saveApproach(int id, double weight, int replay) {
         approaches.add(new Approach(id, weight, replay));
+    }
+
+    public Observable<List<Approach>> observeApproaches() {
+        return null;
     }
 }
